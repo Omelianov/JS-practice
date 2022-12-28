5fe660518d62f5392713a49dcca8eb860241d3c4
Omelianov Nicolai <omelianov.n@gmail.com>
2022-12-28 18:09:05 +0200

    aded-falsy-values

--- index.js
+++ index.js
@@ -2,4 +2,5 @@
 // import "./js/primitives.js";
 // import "./js/to-string.js";
 // import "./js/to-number.js";
-import "./js/to-boolean.js";
+// import "./js/to-boolean.js";
+import "./js/falsy-values.js";
