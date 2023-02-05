718dd054b3054785d4630313cf51cbfb6c64e96e
Omelianov Nicolai <omelianov.n@gmail.com>
2023-02-06 00:47:07 +0200

    Update callback-func.js

--- js/callback-func.js
+++ js/callback-func.js
@@ -101,13 +101,205 @@
 
 // Дополни код так, чтобы в переменной planetsLengths получился массив длин названий планет.Обязательно используй метод map().
 
-const planets = ["Earth", "Mars", "Venus", "Jupiter"];
-// Change code below this line
-const planetsLengths = [];
-planets.map(planet => { planetsLengths.push(planet.length) });
+// const planets = ["Earth", "Mars", "Venus", "Jupiter"];
+// // Change code below this line
+// const planetsLengths = [];
+// planets.map(planet => { planetsLengths.push(planet.length) });
 
-console.log(planetsLengths);
+// console.log(planetsLengths);
 
 // or
 
 // const planetsLengths = planets.map(planet => planet.length);
+
+
+
+// const makeSheff = function (name) {
+//     const makeDish = function (dish) {
+//         console.log(`${name} готовит ${dish}`);
+//     };
+
+//     return makeDish;
+// };
+
+// const mango = makeSheff('Mango');
+
+// mango('сырники');
+
+
+// const poly = makeSheff('Poly');
+
+// poly('кофейку');
+
+
+// !======================Task============================================
+
+
+
+// Используя метод flatMap() сделай так, чтобы в переменной genres 
+// получился массив всех жанров книг(свойство genres) из массива книг books.
+
+
+// const books = [
+//     {
+//         title: "The Last Kingdom",
+//         author: "Bernard Cornwell",
+//         genres: ["adventure", "history"],
+//     },
+//     {
+//         title: "Beside Still Waters",
+//         author: "Robert Sheckley",
+//         genres: ["fiction"],
+//     },
+//     {
+//         title: "Redder Than Blood",
+//         author: "Tanith Lee",
+//         genres: ["horror", "mysticism"],
+//     },
+// ];
+// // Change code below this line
+// const genres = books.map(book => book.genres);
+// // const genres = books.flatMap(book => book.genres);
+// console.log(genres);
+
+
+
+// !======================Task============================================
+
+// const getUserNames = users => {
+//     return users.map(user => user.name
+
+//     )
+// };
+
+// !======================Task============================================
+
+// Дополни код так, чтобы в переменной evenNumbers получился массив чётных 
+// чисел из массива numbers, а в переменной oddNumbers массив нечётных.
+// Обязательно используй метод filter().
+
+
+// const numbers = [17, 24, 82, 61, 36, 18, 47, 52, 73];
+// // Change code below this line
+
+// const evenNumbers = numbers.filter(number => number % 2 === 0);
+// const oddNumbers = numbers.filter(number => number % 2 !== 0);
+
+
+// console.log(evenNumbers);
+// console.log(oddNumbers);
+
+
+
+// !======================Task============================================
+
+// const books = [
+//     {
+//         title: "The Last Kingdom",
+//         author: "Bernard Cornwell",
+//         genres: ["adventure", "history"],
+//     },
+//     {
+//         title: "Beside Still Waters",
+//         author: "Robert Sheckley",
+//         genres: ["fiction", "mysticism"],
+//     },
+//     {
+//         title: "Redder Than Blood",
+//         author: "Tanith Lee",
+//         genres: ["horror", "mysticism", "adventure"],
+//     },
+// ];
+// // Change code below this line
+// const allGenres = books.flatMap(book => book.genres);
+// console.log(allGenres);
+// // const uniqueGenres = allGenres.filter((genre) => uniqueGenres.indexOf(genre) === genre);
+// // const uniqueGenres = [...new Set(allGenres)];
+
+
+
+// // or
+
+// const uniqueGenres = allGenres.filter((genre, index, allGenres) => allGenres.indexOf(genre) === index);
+
+// console.log(uniqueGenres);
+
+
+// !======================Task============================================
+
+// Дополни функцию getUsersWithAge(users, minAge, maxAge) так, чтобы она 
+// возвращала массив пользователей, возраст которых(свойство age) попадает в промежуток от minAge до maxAge.
+
+// const getUsersWithAge = (users, minAge, maxAge) => {
+
+//     return users.filter(user => user.age > minAge && user.age < maxAge)
+// };
+
+
+// !======================Task============================================
+
+// Дополни функцию getUsersWithFriend(users, friendName) так, чтобы она возвращала 
+// массив пользователей у которых есть друг с именем в параметре friendName.
+// Массив друзей пользователя хранится в свойстве friends.
+
+// // Change code below this line
+// const getUsersWithFriend = (users, friendName) => {
+
+//     return users.filter(user => user.friends.includes(friendName))
+// };
+// // Change code above this line
+
+
+
+
+
+// !======================Task============================================
+
+
+
+
+
+// !======================Task============================================
+
+
+
+
+
+// !======================Task============================================
+
+
+
+
+
+
+// !======================Task============================================
+
+
+
+
+
+
+// !======================Task============================================
+
+
+
+
+
+
+// !======================Task============================================
+
+
+
+
+
+
+// !======================Task============================================
+
+
+
+
+
+
+// !======================Task============================================
+
+
