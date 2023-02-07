3e5f2447cb9ee0b95bba34b2a89b9410938bd421
Omelianov Nicolai <omelianov.n@gmail.com>
2023-02-07 12:14:02 +0200

    Update callback-func.js

--- js/callback-func.js
+++ js/callback-func.js
@@ -254,20 +254,46 @@
 
 
 // !======================Task============================================
+// Нашему сервису необходимо рассчитать среднее время проведённое в одной игре 
+// для каждого игрока, и получить общую сумму этих времён.Рассчитать время для 
+// каждого из игроков, можно разделив его время(свойство playtime) на количество
+// игр(свойство gamesPlayed).
+
+// const players = [
+//     { name: "Mango", playtime: 1270, gamesPlayed: 4 },
+//     { name: "Poly", playtime: 469, gamesPlayed: 2 },
+//     { name: "Ajax", playtime: 690, gamesPlayed: 3 },
+//     { name: "Kiwi", playtime: 241, gamesPlayed: 1 },
+// ];
+// // Change code below this line
+
+// const totalAveragePlaytimePerGame = players.reduce((acc, player) => {
+//     return acc + player.playtime / player.gamesPlayed;
+// }, 0);
+// console.log(totalAveragePlaytimePerGame);
 
 
+// !======================Task============================================
+// Дополни функцию calculateTotalBalance(users) так, чтобы она считала и 
+// возвращала сумму всех средств(свойство balance) которые хранят пользователи 
+// из массива users.
+
+const calculateTotalBalance = users =>
+    users.reduce((acc, user) => {
+        return acc + user.balance
+    }, 0);
 
 
 
 // !======================Task============================================
+// Дополни функцию getTotalFriendCount(users) так, чтобы она считала и 
+// возвращала общее количество друзей(свойство friends) всех пользователей 
+// из массива users.
 
-
-
-
-
-// !======================Task============================================
-
-
+// const getTotalFriendCount = users =>
+//     users.reduce((acc, user) => {
+//         return acc + user.friends.length
+//     }, 0);
 
 
 
