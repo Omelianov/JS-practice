0dfa5210f7921405e04ef07491310184ed89d59a
Omelianov Nicolai <omelianov.n@gmail.com>
2023-02-10 00:23:46 +0200

    class tasks

--- js/this-prototype.js
+++ js/this-prototype.js
@@ -86,7 +86,62 @@ console.log(pizzaPalace.order("Big Mike"));
 
 
 // !======================Task============================================
+class Car {
+    // Change code below this line
+    static MAX_PRICE = 50000;
+    #price;
 
+    constructor({ price }) {
+        this.#price = price;
+
+    }
+
+    get price() {
+        return this.#price;
+    }
+
+    set price(newPrice) {
+        if (newPrice <= Car.MAX_PRICE) { this.#price = newPrice };
+
+    }
+    // Change code above this line
+}
+
+const audi = new Car({ price: 35000 });
+console.log(audi.price); // 35000
+
+audi.price = 49000;
+console.log(audi.price); // 49000
+
+audi.price = 51000;
+console.log(audi.price); // 49000
+
+
+
+
+
+// !======================Task============================================
+class Car {
+    static #MAX_PRICE = 50000;
+    // Change code below this line
+
+    static checkPrice(price) {
+
+        if (price > Car.#MAX_PRICE) return "Error! Price exceeds the maximum";
+        return "Success! Price is within acceptable limits"
+    }
+
+    // Change code above this line
+    constructor({ price }) {
+        this.price = price;
+    }
+}
+
+const audi = new Car({ price: 36000 });
+const bmw = new Car({ price: 64000 });
+
+console.log(Car.checkPrice(audi.price)); // "Success! Price is within acceptable limits"
+console.log(Car.checkPrice(bmw.price)); // "Error! Price exceeds the maximum"
 
 
 
@@ -94,21 +149,72 @@ console.log(pizzaPalace.order("Big Mike"));
 
 // !======================Task============================================
 
+class User {
+    constructor(email) {
+        this.email = email;
+    }
+
+    get email() {
+        return this.email;
+    }
+
+    set email(newEmail) {
+        this.email = newEmail;
+    }
+}
+// Change code below this line
+
+class Admin extends User {
+    static AccessLevel = {
+        BASIC: "basic",
+        SUPERUSER: "superuser"
+    }
+}
+
 
 
 
 
 // !======================Task============================================
 
+class User {
+    email;
 
+    constructor(email) {
+        this.email = email;
+    }
 
+    get email() {
+        return this.email;
+    }
 
+    set email(newEmail) {
+        this.email = newEmail;
+    }
+}
 
+class Admin extends User {
+    // Change code below this line
+    constructor({ email, accessLevel }) {
+        super(email, accessLevel);
+        User.AccessLevel = accessLevel;
+    }
 
-// !======================Task============================================
+    static AccessLevel = {
+        BASIC: "basic",
+        SUPERUSER: "superuser",
+    };
 
+    // Change code above this line
+}
 
+const mango = new Admin({
+    email: "mango@mail.com",
+    accessLevel: Admin.AccessLevel.SUPERUSER,
+});
 
+console.log(mango.email); // "mango@mail.com"
+console.log(mango.accessLevel); // "superuser"
 
 
 
